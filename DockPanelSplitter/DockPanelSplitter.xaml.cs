08cc15cc84d9ad2c62b3f8ed0c007a85342066d0
John Burak <john.burak@gmail.com>
2020-01-13 12:40:30 -0700

    On master: Implement DockPanelSplitter and demo app.

---
+++ DockPanelSplitter/DockPanelSplitter.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace System.Windows.Controls
+{
+    /// <summary>
+    ///   Like <see cref="GridSplitter"/>, but for <see cref="DockPanel"/>
+    ///   instead of <see cref="Grid"/>.
+    /// </summary>
+    public partial class DockPanelSplitter : Thumb
+    {
+        FrameworkElement previousSibling;
+        double? initialLength;
+        bool isHorizontal;
+        bool isBottomOrRight;
+
+
+        /// <summary> </summary>
+        public DockPanelSplitter()
+        {
+            InitializeComponent();
+
+            Loaded += OnLoaded;
+            MouseDoubleClick += OnMouseDoubleClick;
+            DragStarted += OnDragStarted;
+            DragDelta += OnDragDelta;
+        }
+
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (!(Parent is DockPanel))
+                throw new InvalidOperationException($"{nameof(DockPanelSplitter)} must be directly in a DockPanel.");
+
+            if (GetPreviousSiblingOrDefault() == default)
+                throw new InvalidOperationException($"{nameof(DockPanelSplitter)} must be directly after a FrameworkElement");
+        }
+
+        void OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+            => SetSiblingLength(initialLength.Value);
+
+        void OnDragStarted(object sender, DragStartedEventArgs e)
+        {
+            var position = DockPanel.GetDock(this);
+            isHorizontal = GetIsHorizontal(position);
+            isBottomOrRight = position == Dock.Bottom || position == Dock.Right;
+            previousSibling = GetPreviousSiblingOrDefault();
+            initialLength ??= GetSiblingLength();
+        }
+
+        void OnDragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var change = isHorizontal ? e.VerticalChange : e.HorizontalChange;
+            if (isBottomOrRight) change = -change;
+
+            var siblingLength = GetSiblingLength();
+            var newSiblingLength = siblingLength + change;
+            if (newSiblingLength < 0) newSiblingLength = 0;
+            newSiblingLength = Math.Round(newSiblingLength);
+
+            SetSiblingLength(newSiblingLength);
+        }
+
+        FrameworkElement GetPreviousSiblingOrDefault()
+        {
+            var parent = Parent as DockPanel;
+            var siblings = parent.Children.OfType<object>();
+            var splitterIndex = parent.Children.IndexOf(this);
+            return siblings.ElementAtOrDefault(splitterIndex - 1) as FrameworkElement;
+        }
+
+        void SetSiblingLength(double length)
+        {
+            if (isHorizontal) previousSibling.Height = length;
+            else previousSibling.Width = length;
+        }
+
+        double GetSiblingLength() => isHorizontal ?
+                                     previousSibling.ActualHeight :
+                                     previousSibling.ActualWidth;
+
+        static bool GetIsHorizontal(Dock position)
+            => position == Dock.Top || position == Dock.Bottom;
+
+
+        internal class CursorConverter : IValueConverter
+        {
+            public static CursorConverter Instance { get; } = new CursorConverter();
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                var position = (Dock)value;
+                var isHorizontal = GetIsHorizontal(position);
+                return isHorizontal ? Cursors.SizeNS : Cursors.SizeWE;
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+                => throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+DockPanelSplitter:
+
+* Is like WPF's [GridSplitter][GridSplitterApi], but for [DockPanel][DockPanelApi] instead of Grid.
+*  Can be double-clicked to reset its position.
+*  Is under the "MIT No Attribution" (MIT-0) License. See [License.txt](License.txt).
+*  Was inspired by "[DockPanel Splitter Control for WPF][InspiredLink]" and the [GridSplitter reference source code][GridSplitterSource].
+
+[GridSplitterApi]: https://docs.microsoft.com/en-us/dotnet/api/system.windows.controls.gridsplitter
+[DockPanelApi]: https://docs.microsoft.com/en-us/dotnet/api/system.windows.controls.dockpanel
+[InspiredLink]: https://www.codeproject.com/Articles/34377/DockPanel-Splitter-Control-for-WPF
+[GridSplitterSource]: https://referencesource.microsoft.com/#PresentationFramework/src/Framework/System/Windows/Controls/GridSplitter.cs
